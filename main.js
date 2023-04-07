cadd8805b377ee7513c827fa77ea8d8ed91d6086
Jadslog <121417622+Jadslog@users.noreply.github.com>
2023-04-07 13:06:16 -0600

    Display Questions JavaScript

--- main.js
+++ main.js
@@ -1,3 +1,38 @@
+// Questions  
+
+const questions = [
+    {
+        question_id: "0",
+        question: "Which of the following is a browswer?",
+        options: [
+            {option: "Java"},
+            {option: "Google Chrome"},
+            {option: "Python"},
+        ],
+        correct_answer: "Google Chrome"
+    },
+    {
+        question_id: "1",
+        question: "What is the full form of CPU?",
+        options: [
+            {option: "Control Processing Unit"},
+            {option: "Combined Processing Unit"},
+            {option: "Central Processing Unit"},
+        ],
+        correct_answer: "Central Processing Unit"
+    },
+    {
+        question_id: "2",
+        question: "CSS stands for Cascading Styleshet?",
+        options: [
+            {option: "True"},
+            {option: "False"},
+        ],
+        correct_answer: "True"
+    },
+]
+
+
 const startQuizBtn = document.querySelector(".start-quiz-btn");
 const quizContainer = document.querySelector(".quiz-container");
 const nextBtn = document.querySelector(".next-btn");
@@ -5,6 +40,7 @@ const quizResultContainer = document.querySelector(".quiz-result-container");
 const quizOverlay = document.querySelector(".quiz-overlay");
 const retakeQuizBtn = document.querySelector(".retake-quiz-btn");
 const quizCloseBtn = document.querySelector(".quiz-close-btn");
+const question = document.querySelector(".question");
  
 //Start Button
 
@@ -34,3 +70,11 @@ retakeQuizBtn.addEventListener("click", () => {
     quizResultContainer.classList.remove("active");
     quizContainer.classList.add("active");
 });
+
+// Display Question
+
+const displayQuestions = (qNo) => {
+    question.innerText = questions[qNo].question;
+}
+
+displayQuestions(2);
