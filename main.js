2cc446f5dc12d819a6a5cd2a4b3a808778cce21e
Jadslog <121417622+Jadslog@users.noreply.github.com>
2023-04-09 01:23:06 -0600

    Display the Result JavaScript

--- main.js
+++ main.js
@@ -42,27 +42,42 @@ const retakeQuizBtn = document.querySelector(".retake-quiz-btn");
 const quizCloseBtn = document.querySelector(".quiz-close-btn");
 const optionsContainer = document.querySelector(".options-container");
 const question = document.querySelector(".question");
+const resultHeading = document.querySelector(".result-heading");
+const scoreText = document.querySelector(".score-text");
 
 let questionNumber = 0;
 let totalQuestion = questions.length;
+let userAnwser = "";
+let userScore = 0;
+
+const init = () => {
+    questionNumber = 0;
+    userScore = 0;
+    nextBtn.innerText = "Next";
+}
 
 //Start Button
 
 startQuizBtn.addEventListener("click", () => {
+    init();
     quizContainer.classList.add("active");
     quizOverlay.classList.add("active");
+    displayQuestions(questionNumber);
 });
 
 //Next Button 
 
 nextBtn.addEventListener("click", () => {
-
+    checkAnswer();
     questionNumber++;
-
+    if (questionNumber == totalQuestion - 1){
+        nextBtn.innerText = "Finish"
+    }
     if (questionNumber < totalQuestion){
         displayQuestions(questionNumber);
     }else {
         quizResultContainer.classList.add("active");
+        showResult();
         quizContainer.classList.remove("active");
     }
 });
@@ -78,9 +93,31 @@ quizCloseBtn.addEventListener("click", () => {
 // Reatake Quiz Button
 
 retakeQuizBtn.addEventListener("click", () => {
+    init();
     quizResultContainer.classList.remove("active");
     quizContainer.classList.add("active");
+    displayQuestions(questionNumber);
 });
+
+// Store Answer
+
+const storeAnwser = (o) => {
+    if (o){
+        userAnwser = o.target.nextElementSibling.innerText;
+        console.log("user " + userAnwser);
+    }else {
+        userAnwser = "";
+    }
+}
+
+// Check Answer 
+
+const checkAnswer = () => {
+    if (userAnwser == questions[questionNumber].correct_answer){
+        userScore++;
+    }
+}
+
 
 // Display Question
 
@@ -92,10 +129,12 @@ const displayQuestions = (qNo) => {
     questions[qNo].options.forEach((o, oIndex) => {
         const optionRadioButton = document.createElement("input");
         optionRadioButton.type = "radio";
+       
         let optionId = "option" + oIndex;
         optionRadioButton.id = optionId;
         optionRadioButton.name = "option";
-
+        optionRadioButton.addEventListener("change",storeAnwser);
+        
         const optionLabel = document.createElement("label");
         optionLabel.htmlFor = optionId;
         optionLabel.classList.add("option");
@@ -107,3 +146,17 @@ const displayQuestions = (qNo) => {
 }
 
 displayQuestions(questionNumber);
+
+// Show the Result
+
+const showResult = () => {
+    let percentage = (userScore/totalQuestion) * 100;
+
+    if (percentage >= 60) {
+        resultHeading.innerText = "Congratulations!";
+    } else {
+        resultHeading.innerText = "You can do better";
+    }
+
+    scoreText.innerText = `You have scored ${userScore} out of ${totalQuestion}.`;  
+}
